220b76d9f1a5328a244a078941034894f8e54280
dotcomstar <gamestar109@live.com>
2023-05-11 15:13:02 -0700

    Build platform dropdown selector

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,6 @@
 import useData from "./useData";
 import { Genre } from "./useGenres";
-
-export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-}
+import { Platform } from "./usePlatforms";
 
 export interface Game {
     id: number;
@@ -15,7 +10,6 @@ export interface Game {
     metacritic: number;
   }
 
- 
 const useGames = (selectedGenre: Genre | null) =>
   useData<Game>('/games', { params: { genres: selectedGenre?.id } }, [
     selectedGenre?.id
