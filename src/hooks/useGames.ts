c1f36fadca6479b3f7258fc6fea7d3ebc673cd2e
dotcomstar <gamestar109@live.com>
2023-05-11 16:37:59 -0700

    Filter games based on platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,16 +3,26 @@ import { Genre } from "./useGenres";
 import { Platform } from "./usePlatforms";
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-  }
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
 
-const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>('/games', { params: { genres: selectedGenre?.id } }, [
-    selectedGenre?.id
-  ]);
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+        parent_platforms: selectedPlatform?.id,
+      },
+    },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
 export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '9c04d5f866334157a3533eca86a06bb8'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "9c04d5f866334157a3533eca86a06bb8",
+  },
+});
