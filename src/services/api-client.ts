9ae3df4ee53898a9082561a6c2ad1d99a2c017d0
dotcomstar <gamestar109@live.com>
2023-05-15 11:50:21 -0700

    Update api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "9c04d5f866334157a3533eca86a06bb8",
+    key: "28f73fc9016f43d3b066b803ea409273",
   },
 });
