fbb058629e3272b0528f953ee3cf2b94424fac2d
dotcomstar <gamestar109@live.com>
2023-06-22 09:07:48 -0700

    Add attribution to Rawg.io

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,14 +3,16 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
+import Hyperlink from "./Hyperlink";
 
 const NavBar = () => {
   return (
-    <HStack padding="10px">
+    <HStack padding="12px">
       <Link to="/">
         <Image src={logo} boxSize="60px" objectFit="cover" />
       </Link>
       <SearchInput />
+      <Hyperlink />
       <ColorModeSwitch />
     </HStack>
   );
